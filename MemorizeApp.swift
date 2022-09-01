b2e69b0480fab7002d8620bcb72ea82a50b83806
Mikhail Ermakov <m.ermakov@youps.io>
2022-09-01 10:50:22 +0300

    Lecture 2

---
+++ MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mikhail on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
